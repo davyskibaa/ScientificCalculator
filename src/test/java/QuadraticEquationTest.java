5c4227bee9415164146725e24effba6d8be31382
davyskibaa <alenadavyskiba@gmail.com>
2022-10-07 18:10:10 +0300

    some changes v1

--- src/test/java/QuadraticEquationTest.java
+++ src/test/java/QuadraticEquationTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class QuadraticEquationTests {
+public class QuadraticEquationTest {
 
     @Test
     public void testOneRootQE() {
